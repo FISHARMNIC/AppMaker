ee375ce394ca398aa99a80c03a3fe9c42b279b29
Nicolas <73864341+FISHARMNIC@users.noreply.github.com>
2021-05-13 21:23:09 -0700

    Add files via upload

---
+++ appcreator/blink.js
@@ -0,0 +1,29 @@
+const UIview = require('./UIview.js')
+
+let page = new UIview('site/index.html')
+
+    page.script(`
+
+        var profile_handle = 0  //create a handle for dealing with multiple blinking objects
+        var blink_states = []   //the states for each blinking object
+
+        function create_blink(id,speed) { //initializer
+            blink_states.push(false) //create a new "user" (blink object)
+            setInterval(function(){return blink(id,profile_handle)},speed); //run blink every "speed" seconds
+            profile_handle += 1
+        }
+
+        function blink(el_id,profile) {
+            blink_states[profile] = !(blink_states[profile]) //toggle the state of the object
+            document.getElementById(el_id).hidden = blink_states[profile] //set the view/hide property accordingly
+        }
+
+        create_blink("sometext",500)
+    `)
+
+    page.Text({
+        value: "blinking text",
+        id: "sometext",
+    })
+
+    page.call()
